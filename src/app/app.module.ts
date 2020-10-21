8958c773b1d0f360b485f73e6f7934f888b15935
Simon Jornet <jornetsimon@gmail.com>
2020-10-21 16:39:23 +0200

    ng-zorro & style utilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { fr_FR, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+registerLocaleData(fr);
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
-	providers: [],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		FlexLayoutModule,
+	],
+	providers: [
+		{ provide: NZ_I18N, useValue: fr_FR },
+		{ provide: LOCALE_ID, useValue: 'fr' },
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
