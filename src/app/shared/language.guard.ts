ba67a8cee24aad5e3feeb78a2592d6599336df1b
Simon Jornet <jornetsimon@gmail.com>
2021-01-09 13:50:59 +0100

    fix(language-guard): allow US & GB english

--- src/app/shared/language.guard.ts
+++ src/app/shared/language.guard.ts
@@ -12,13 +12,24 @@ import { Observable } from 'rxjs';
 	providedIn: 'root',
 })
 export class LanguageGuard implements CanActivateChild {
-	private allowedLanguageTags: ReadonlyArray<string> = ['fr', 'fr-FR', 'fr-BE', 'fr-CH', 'fr-LU'];
+	private allowedLanguageTags: ReadonlyArray<string> = [
+		'fr',
+		'fr-FR',
+		'fr-BE',
+		'fr-CH',
+		'fr-LU',
+		'en',
+		'en-US',
+		'en-GB',
+	].map((x) => x.toLowerCase());
 	constructor(private router: Router) {}
 	canActivateChild(
 		childRoute: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
 	): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-		if (!navigator.languages.some((tag) => this.allowedLanguageTags.includes(tag))) {
+		if (
+			!navigator.languages.some((tag) => this.allowedLanguageTags.includes(tag.toLowerCase()))
+		) {
 			return this.router.parseUrl('/sorry');
 		}
 		return true;
