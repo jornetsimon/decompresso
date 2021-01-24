018a267e8ed0054750fa2b98a57efff03ab8f396
Simon Jornet <jornetsimon@gmail.com>
2021-01-24 09:59:12 +0100

    feat(help): highlight the fragment

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 @Component({
 	selector: 'mas-help',
@@ -7,7 +9,18 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 	styleUrls: ['./help.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HelpComponent {
+export class HelpComponent implements AfterViewInit {
 	isTouchDevice = this.deviceDetectorService.isMobile() || this.deviceDetectorService.isTablet();
-	constructor(private deviceDetectorService: DeviceDetectorService) {}
+	constructor(
+		private deviceDetectorService: DeviceDetectorService,
+		private route: ActivatedRoute
+	) {}
+
+	ngAfterViewInit() {
+		this.route.fragment.pipe(first()).subscribe((fragment) => {
+			if (fragment) {
+				(document.querySelector(`#${fragment}`) as HTMLElement).classList.add('target');
+			}
+		});
+	}
 }
