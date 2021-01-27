d150aca9ceb3026a09d409197205a6db52e05a96
Simon Jornet <jornetsimon@gmail.com>
2021-01-27 11:47:01 +0100

    about: team pictures

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
 	declarations: [AboutComponent],
-	imports: [CommonModule, RouterModule.forChild([{ path: '', component: AboutComponent }])],
+	imports: [
+		CommonModule,
+		SharedModule,
+		RouterModule.forChild([{ path: '', component: AboutComponent }]),
+	],
 })
 export class AboutModule {}
